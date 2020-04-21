80150e71b71b6c0f508e7bc87b0bf10a6685a90f
Kire Semada <egadames990@gmail.com>
2020-04-21 08:36:36 -0700

    added comments, updated README.md and fixed errors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+// add the dependencies need to run the js file
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const render = require("./lib/htmlRenderer");
 const writeFile = require('./lib/writeFile');
-const axios = require("axios");
 
+// This function is ran when a new team member needs to be added
 function partialTeamPrompt() {
+  // The prompt to ask for a new team member
   inquirer
     .prompt([
       {
@@ -16,6 +18,8 @@ function partialTeamPrompt() {
         choices: ["Engineer", "Intern", "Done"]
       },
     ])
+    // This function checks what team member needs to be added
+    // then runs the corresponding prompt
     .then((answers) => {
       switch (answers.team) {
         case "Engineer":
@@ -25,53 +29,69 @@ function partialTeamPrompt() {
             internPrompt();
         break;
         case "Done":
+          // This runs the function that creates the string 
+          // that needs to be loaded into the team.html
            const bigAssString = render(employees);
+           // This function writes the team.html into the output folder
            writeFile(bigAssString);
         break;
       }
     });
 }
 
+// This function is used to validate that a string is used
 const confirmString = (answers) => {
-let str = /[A-Za-z]/g;
+// This creates a variable that contains all letters uppercase, lowercase and a space
+let str = /[A-Za-z ]/g;
+// This method will match any letters or space with the answer given by the user
+// creates an array containing the matches
 let matches_array = answers.match(str);
+  // This checks if there is anything else submitted by the user
   if(matches_array == null || matches_array.length < answers.length) {
-    return 'Please enter a name. It can only contain letters';
+    // If user did not use a string it loads this message for them to update
+    return 'Please enter a valid response. It can only contain letters';
   }
+  // if no issues it returns true and prompt continues
   return true;
 }
 
+// This function is used to validate that only numbers are used
 const confirmNumber = (answers) => {
+  // This creates a variable that contains all numbers
   let num = /[0-9]/g;
+  // This method will match any number with the answer given by the user
+  // creates an array containing the matches
   let matches_array = answers.match(num);
+    // This checks if there is anything else submitted by the user
     if(matches_array == null || matches_array.length < answers.length) {
-      return 'Please enter a valid ID. It can only contain numbers';
+      // If user did not use only numbers it loads this message for them to update
+      return 'Please enter a valid response. It can only contain numbers';
     }
+    // if no issues it returns true and prompt continues
     return true;
 }
 
+// This function is used to validate that an email is submitted
+// Due to the complicated nature of email this is a simple check
 const confirmEmail = (answers) => {
+  // This creates a variable that contain @ which is needed in an email
   let special = /[@]/g;
+  // This method will match @ with the answer given by the user
+  // creates an array containing the matches
   let matches_array1 = answers.match(special);
+  // This checks if @ was used at least only once
   if(matches_array1 == null || matches_array1.length != 1) {
+    // If user did not respond correctly it loads this message for them to update
     return 'Please enter a valid Email. It must contain at least one @';
   }
+    // if no issues it returns true and prompt continues
     return true;
 }
 
-// const confirmUserName = async (answers) => {
-//   const queryUrl = `https://api.github.com/users/${answers}/events`;
-//   let response = 
-//    await axios.get(queryUrl)
-//     if(response.data.length > 0)
-//         return true;
-//       else{
-//         return false;
-//       }
-// }
-
+// This is the prompt for the manager
 function managerPrompt() {
- return inquirer.prompt([
+  // this gathers all the information to be loaded into the manager class
+  inquirer.prompt([
     {
       type: "input",
       name: "managerName",
@@ -97,20 +117,24 @@ function managerPrompt() {
       validate: confirmNumber
     },
   ]).then((answers) => {
+    // This loads the user's response into a new Manager class
     const manager = new Manager(
       answers.managerName,
       answers.managerId,
       answers.managerEmail,
       answers.managerOfficeNum
     );
+    //This pushes the new class into the employees array
     employees.push(manager);
+    // This reloads the prompt to ask if a new member is needed
     partialTeamPrompt();
   });
 }
 
+// This is the prompt for the engineer
 function engineerPrompt() {
-  inquirer
-    .prompt([
+  // this gathers all the information to be loaded into the engineer class
+  inquirer.prompt([
       {
         type: "input",
         name: "engineerName",
@@ -133,22 +157,26 @@ function engineerPrompt() {
         type: "input",
         name: "engineerUserName",
         message: "What is your engineer's GitHub username?"
-        // validate: confirmUserName
       },
     ])
     .then((answers) => {
+      // This loads the user's response into a new Engineer class
       const engineer = new Engineer(
         answers.engineerName,
         answers.engineerId,
         answers.engineerEmail,
         answers.engineerUserName
       );
+      //This pushes the new class into the employees array
       employees.push(engineer);
+      // This reloads the prompt to ask if a new member is needed
       partialTeamPrompt();
     });
 }
 
+// This is the prompt for the intern
 function internPrompt() {
+  // this gathers all the information to be loaded into the intern class
   inquirer.prompt([
     {
       type: "input",
@@ -175,18 +203,22 @@ function internPrompt() {
       validate: confirmString
     },
   ]).then((answers) => {
+    // This loads the user's response into a new Intern class
     const intern = new Intern(
       answers.internName,
       answers.internId,
       answers.internEmail,
       answers.internSchool
     );
+    //This pushes the new class into the employees array
     employees.push(intern);
+    // This reloads the prompt to ask if a new member is needed
     partialTeamPrompt();
   });
 }
-
+// This creates the array that will contain all the classes
 let employees = [];
 
+// This starts the function that will get a manager
 managerPrompt();
 
